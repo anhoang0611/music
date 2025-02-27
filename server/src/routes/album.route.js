efdefb8829215e0bcd246073f6970f2c02fff677
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-27 11:28:07 +0700

    album route and controller

--- server/src/routes/album.route.js
+++ server/src/routes/album.route.js
@@ -1,13 +1,9 @@
 import { Router } from "express";
-// import { getAlbumById, getAllAlbums } from "../controller/album.controller.js";
+import { getAlbumById, getAllAlbums } from "../controller/album.controller.js";
 
 const router = Router();
 
-// router.get("/", getAllAlbums);
-// router.get("/:albumId", getAlbumById);
-
-router.get("/", (req, res) => {
-  res.send("Hello World");
-});
+router.get("/", getAllAlbums);
+router.get("/:albumId", getAlbumById);
 
 export default router;

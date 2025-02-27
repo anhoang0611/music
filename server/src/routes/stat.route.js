7943c01e3ee7b2827201bb0fb586e9d0cd20df7e
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-27 12:27:13 +0700

    user router and controller

--- server/src/routes/stat.route.js
+++ server/src/routes/stat.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-// import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
-// import { getStats } from "../controller/stat.controller.js";
+import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
+import { getStats } from "../controller/stat.controller.js";
+import { Song } from "../models/song.model.js";
+import { User } from "../models/user.model.js";
+import { Album } from "../models/album.model.js";
 
 const router = Router();
 
-// router.get("/", protectRoute, requireAdmin, getStats);
+router.get("/", protectRoute, requireAdmin, getStats);
 
 export default router;
